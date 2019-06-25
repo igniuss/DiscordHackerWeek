ef416e9e911fea72682a90ace8c9fb1f1972e22f
Jeffery Gray <jgray0705@gmail.com>
2019-06-25 11:08:28 -0700

    Added character classes

---
+++ RPGBot/Characters/Thief.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPGBot.Characters {
+    // Thief has average attack and health, but gets a bonus on gold earned
+    public class Thief : CharacterBase {
+        public override float AttackPowerMultiplier {
+            get { return 1; }
+        }
+
+        public override float HealthMultiplier {
+            get { return 1; }
+        }
+
+        public override float GoldMultiplier {
+            get { return 1.5f; }
+        }
+    }
+}
